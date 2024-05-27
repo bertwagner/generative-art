4c90123ba2b75ee995a6a9446a44801dc37bf16d
Bert Wagner <bertwagner@bertwagner.com>
2024-05-27 12:28:12 -0400

    get basic object control working

--- 03-circle-objects/sketch.js
+++ 03-circle-objects/sketch.js
@@ -1,38 +1,51 @@
+let fps=60;
+let tc;
+
 function setup() {
- createCanvas(800,800);
+ createCanvas(400,400);
  angleMode(DEGREES)
- frameRate(30);
+ frameRate(fps);
+
+ tc = new TraceCircle(100,100,50,10,"red",15);
+}
+
+class TraceCircle {
+  constructor(x_center,y_center,diameter,strokeSize,strokeColor,rotationsPerMinute) {
+    this.x=x_center;
+    this.y=y_center;
+    this.d=diameter;
+    this.strokeSize=strokeSize;
+    this.strokeColor=strokeColor;
+    this.angle=0;
+    this.rpm=rotationsPerMinute;
+    // convert from rpm and fps to the increment angle of each frame
+    this.angle_i=360*((this.rpm/60)/fps);
+  }
+
+  move() {
+    this.angle=this.angle+this.angle_i
+  }
+
+  displayBackground(color) {
+    noStroke();
+    fill(color);
+    circle(this.x,this.y,this.d)
+  }
+
+  display() {
+    noStroke();
+    fill(this.strokeColor);
+    circle(this.x+(cos(this.angle)*(this.d/2)),this.y+(sin(this.angle)*(this.d/2)),this.strokeSize);
+  }
+
+
 }
 
 
 function draw() {
-  background("grey");
+  background(128);
 
-  
-  // draw grey circles
-  for (var m=0; m<matrix.length; m++) {
-    // strokeColor("grey");
-    // strokeSize(3);
-    var grey = color(255,255,255)
-    grey.setAlpha(40)
-    fill(grey)
-    circle(matrix[m][0],matrix[m][1],50);
-   }
-
-   //draw each orbiting circle and it's ghosted trail
-   for (var m=0; m<matrix.length; m++) {
-    let c=color("red")
-
-
-    c.setAlpha(255*(1/3));
-    scribeCircle(matrix[m][0],matrix[m][1],50,16,c,(i-(d*128)));
-
-    c.setAlpha(255*(2/3));
-    scribeCircle(matrix[m][0],matrix[m][1],50,18,c,(i-d*64));
-
-    c.setAlpha(255);
-    scribeCircle(matrix[m][0],matrix[m][1],50,20,c,i);
-
-    i=i+d
-  }
+  tc.move();
+  tc.displayBackground(60);
+  tc.display();
 }
